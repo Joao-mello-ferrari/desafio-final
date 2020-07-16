dc22fe9278e7986079285691ae40dfb142d236dd
joao.mello <joao.vico.mellof@gmail.com>
2020-07-16 14:22:42 -0300

    Ajuste da rota da API

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   // baseURL: 'http://localhost:3001/',
-  baseURL: 'https://desafio-final-fullstack.herokuapp.com/api/',
+  baseURL: 'https://desafio-final-fullstack.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
